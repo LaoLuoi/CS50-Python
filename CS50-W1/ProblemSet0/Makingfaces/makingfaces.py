1e0689b07d542dbe70742dd76f4b5819e4d3cabf
Tien Le <127403474+LaoLuoi@users.noreply.github.com>
2025-04-15 01:16:18 +0000

    Update makingfaces.py

--- CS50-W1/ProblemSet0/Makingfaces/makingfaces.py
+++ CS50-W1/ProblemSet0/Makingfaces/makingfaces.py
@@ -1,1 +1,11 @@
+def convert(user_input):    # function convert
+    modified_input = user_input.replace(":)", "🙂").replace(":(", "🙁") # convert emoticons to emoji
+    return modified_input
 
+def main():   # function main
+    user_input = input("Please enter a smiley or frowney face: ")    # prompts the user for input
+    convert(user_input)    # call function convert
+    print(user_input)    # print result
+
+if __name__ == "__main__":
+    main()  # call function main
