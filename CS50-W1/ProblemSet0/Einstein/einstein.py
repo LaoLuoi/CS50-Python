d25a8089eb506afac63524a2fb59ec3bc9c025a8
Tien Le <127403474+LaoLuoi@users.noreply.github.com>
2025-04-15 01:50:41 +0000

    Update einstein.py

--- CS50-W1/ProblemSet0/Einstein/einstein.py
+++ CS50-W1/ProblemSet0/Einstein/einstein.py
@@ -1,1 +1,8 @@
+def main():     # Funtion main
+    mass = int(input("Enter a mass in kilograms: "))      # Input mass (in kilograms)
+    c = 300000000       # c: speed of light (meters per second)
+    e = mass * (c ** 2)     # Energy formula (in Joules)
+    print(int(e))       # Print integer e
 
+if __name__ == "__main__":
+    main()      # Call function main
