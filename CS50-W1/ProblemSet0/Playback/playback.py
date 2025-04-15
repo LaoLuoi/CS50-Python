a5c4846b39ad5d6283a088a698758342feb9d02a
Tien Le <127403474+LaoLuoi@users.noreply.github.com>
2025-04-15 00:19:15 +0000

    Update playback.py

--- CS50-W1/ProblemSet0/Playback/playback.py
+++ CS50-W1/ProblemSet0/Playback/playback.py
@@ -1,1 +1,6 @@
+# Defines a function playback
+def playback():
+    mesage = input("What do you wanna say?: ") # Input
+    print(mesage.strip().replace(" ", "...")) # Strip and replace input
 
+playback() # Call function playback
