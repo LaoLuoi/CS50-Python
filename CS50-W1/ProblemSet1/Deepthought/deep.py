2b1f8b57fd08a18446aa5ff454909f621552b7d5
Tien Le <127403474+LaoLuoi@users.noreply.github.com>
2025-04-22 17:05:30 +0700

    Update deep.py

--- CS50-W1/ProblemSet1/Deepthought/deep.py
+++ CS50-W1/ProblemSet1/Deepthought/deep.py
@@ -1,1 +1,16 @@
+def main():
+    # prompt the user for the answer
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    # Call function
+    check(answer)
 
+def check(a):
+    # Case- and space-insensitively.
+    a = a.lower().strip()
+    # Conditions of deep thuoght
+    if a == "42" or a == "forty-two" or a == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+main()
